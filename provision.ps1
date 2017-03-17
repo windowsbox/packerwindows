a837e26132ef2732c022f5c8fd36859e1f9ff830
Shawn Neal <sneal@sneal.net>
2017-03-17 12:35:32 -0700

    collapse dev and regular build into single config

--- provision.ps1
+++ provision.ps1
@@ -8,14 +8,37 @@ Install-Module WindowsBox.RDP -Force
 Install-Module WindowsBox.UAC -Force
 Install-Module WindowsBox.VagrantAccount -Force
 Install-Module WindowsBox.VMGuestTools -Force
-Install-Module WindowsBox.DevTools -Force
 
 Disable-AutoLogon
 Disable-UAC
 Enable-RDP
-Enable-DevTools
 Set-ExplorerConfiguration
 Disable-Hibernation
 Set-VagrantAccount
 Install-VMGuestTools
+
+if ($env:devtools -eq $true) {
+  # Install Linux subsystem
+  Install-Module WindowsBox.DevMode -Force
+  Enable-DevMode
+
+  # Install chocolatey and use it to install dev tools
+  Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+
+  choco install powershell-packagemanagement -y
+  choco install visualstudio2017community -y
+  choco install resharper -pre -y
+  choco install notepadplusplus.install -y
+  choco install googlechrome -y
+  choco install git.install -y
+  choco install beyondcompare -y
+  choco install fiddler4 -y
+  choco install sql-server-management-studio -y
+  choco install nodejs.install -y
+  choco install cloudfoundry-cli -y
+  choco install nuget.commandline -y
+  choco install soapui -y
+}
+
+# Final cleanup
 Optimize-DiskUsage
