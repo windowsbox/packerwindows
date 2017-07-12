20218e366d2e693b5c0e1cf32e2433cdaa94f239
Shawn Neal <sneal@sneal.net>
2017-07-12 16:43:55 -0700

    Add install-packageprovider nuget
    Win10 anniversary update supports install-packageprovider which is the mainstream way of installing package providers, so let's do that.

--- preprovision.ps1
+++ preprovision.ps1
@@ -1,8 +1,7 @@
 # Runs before Packer provisioners during first boot
 
 # We explicitly install nuget because install-module prompts for confirmation even with -Force
-# and Install-PackageProvider is not available on Win10 by default
-Install-Package Nuget -Force
+Install-PackageProvider -Name Nuget -Force
 
 # Install and execute Windows updates and start WinRM when complete
 Install-Module WindowsBox.Network -Force
